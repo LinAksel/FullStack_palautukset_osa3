ff02a8f13539c92428b14c4ac82304e1fc09ec6e
Aksel Linros <aksel.linros@nitor.com>
2023-07-01 13:40:04 +0300

    task 3.1-3.6: basic phone book backend

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = require("./persons.json");
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const date = new Date();
+  const info = `<div>Phonebook has info for ${
+    Object.keys(persons).length
+  } people</div><div>${date}</div>`;
+  res.send(info);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generatePersonId = () => {
+  min = Math.ceil(1);
+  max = Math.floor(10000000);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  } else if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    id: generatePersonId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
